3b7a69acef9934a832fdd966a8a82b36ea0b5813
Guillaume Polaert <guillaume@logmatic.io>
2016-04-14 11:13:09 +0200

    generalize Endpoints

---
+++ src/main/java/io/logmatic/asynclogger/net/Endpoint.java
@@ -0,0 +1,14 @@
+package io.logmatic.asynclogger.net;
+
+/**
+ * Created by gpolaert on 4/14/16.
+ */
+public interface Endpoint {
+    boolean send(String data);
+
+    boolean isConnected();
+
+    void closeConnection();
+
+    boolean openConnection();
+}
