ebce64725026ee32d31929da619467557c043265
Nicolas Ygrand <nicolas.ygrand@fabernovel.com>
2022-05-18 16:27:56 +0200

    Replace legacy libraries with AndroidX

--- logmatic/src/main/java/io/logmatic/android/Logger.java
+++ logmatic/src/main/java/io/logmatic/android/Logger.java
@@ -1,9 +1,9 @@
 package io.logmatic.android;
 
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.util.ArrayMap;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.collection.ArrayMap;
 import android.util.Log;
 
 import com.google.gson.FieldNamingPolicy;
